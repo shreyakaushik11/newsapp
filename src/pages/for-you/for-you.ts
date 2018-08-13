7af12329920313468b2a074a57817e47f8957421
Harshita Mathur <hmathurjaipur@gmail.com>
2018-08-13 17:04:42 +0530

    dfjdhdasyu

--- src/pages/for-you/for-you.ts
+++ src/pages/for-you/for-you.ts
@@ -18,24 +18,53 @@ import {DataProvider} from '../../providers/data/data';
 export class ForYouPage {
   title;
   data1;
+  data;
+  img;
+  i;
 
   constructor(public navCtrl: NavController, public navParams: NavParams,private http:HttpClient,public data:DataProvider) {}
   
 url='https://newsapi.org/v2/top-headlines?country=in&apiKey=7c48c5e4372b458682302a0b9fdd44d8';
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ForYouPage');
-  }
-  getdata(){
+    this.http.get(this.url).subscribe(data => {
+      for(this.i=0;this.i<data.articles.length;this.i++)
+      {
+        this.data = data;
+      this.title=data.articles[this.i].title;
+      this.img=data.articles[this.i].urlToImage;
+      console.log(data);
+      console.log(this.title);
+      console.log(this.img)
+    }
+    }, err => {
+      console.log(err);
+      
+    });
+  
+  
 
-      this.http.get(this.url).subscribe(data => {
-        this.title=data.articles[0].title;
-        console.log(data);
-        //this.title=JSON.parse(JSON.stringify(data.articles[0]));
+ 
     
-      }, err => {
-        console.log(err);
+    
+   
+  
+  }
+  // getdata(){
+
+  //     this.http.get(this.url).subscribe(data => {
+  //       for(this.i=0;this.i<data.articles.length;this.i++)
+  //       {
+  //         this.data = data;
+  //       this.title=data.articles[this.i].title;
+  //       this.img=data.articles[this.i].urlToImage;
+  //       console.log(data);
+  //       console.log(this.title);
+  //       console.log(this.img)
+  //     }
+  //     }, err => {
+  //       console.log(err);
         
-      });
+  //     });
     
     
   
@@ -43,6 +72,7 @@ url='https://newsapi.org/v2/top-headlines?country=in&apiKey=7c48c5e4372b45868230
       
       
      
-    }}
+  //   }}
+}
   
 
